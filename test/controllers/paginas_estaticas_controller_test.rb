9f1ed5a71b31668dce8a40393ddd1e1e636a2489
jeffesson <jeffesson75@hotmail.com>
2020-02-07 00:41:55 -0200

    Adicionar título dinâmico nas páginas estáticas

--- test/controllers/paginas_estaticas_controller_test.rb
+++ test/controllers/paginas_estaticas_controller_test.rb
@@ -1,19 +1,28 @@
 require 'test_helper'
 
+def setup
+  @titulo_base = "Terceiro App Web II"
+  end
+  
+
 class PaginasEstaticasControllerTest < ActionDispatch::IntegrationTest
   test "should get inicio" do
     get paginas_estaticas_inicio_url
     assert_response :success
-  end
+    assert_select "inicio" , "Início | #{@titulo_base}"
+    end
+    
 
   test "should get ajuda" do
     get paginas_estaticas_ajuda_url
     assert_response :success
+    assert_select "ajuda" , "Ajuda | #{@titulo_base}"
   end
 
   test "should get sobre" do
     get paginas_estaticas_sobre_url
     assert_response :success
+    assert_select "sobre" , "Sobre | #{@titulo_base}"
   end
 
   
